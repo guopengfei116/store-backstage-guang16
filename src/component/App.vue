d023650fae8e87d8de8804ac4ce5a8aaa08d2a24
PengFei <626138959@qq.com>
2018-02-27 17:29:22 +0800

    集成elementUI

--- src/component/App.vue
+++ src/component/App.vue
@@ -3,6 +3,12 @@
         {{ val }}
         <!-- 根组件路由占位, 将来通过url来控制页面的展示 -->
         <router-view></router-view>
+
+        <el-switch
+            v-model="nian"
+            active-text="去年"
+            inactive-text="今年">
+        </el-switch>
     </div>
 </template>
 
@@ -11,7 +17,8 @@
         // 这是es6中对象定义方法的语法
         data() {
             return {
-                val: 'vue脚手架测试'
+                val: 'vue脚手架测试',
+                nian: true
             }
         },
     }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from './component/App.vue'
 
 import vueRouter from './router'  // 自动导入该目录下的index.js
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
 new Vue({
     el: '#app',
     // 下面使用的箭头函数也是es6语法
