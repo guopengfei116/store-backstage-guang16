70f306c0d1fc38e6624ee24ac1333a573f6bdb9a
PengFei <626138959@qq.com>
2018-03-02 11:59:28 +0800

    完成左侧导航

--- src/component/admin/subcom/Aside.vue
+++ src/component/admin/subcom/Aside.vue
@@ -1,12 +1,54 @@
 <template>
     <div>
-        公共左侧组件
+        <el-menu class="el-menu-vertical-demo" background-color="#545c64"
+            text-color="#fff" active-text-color="#ffd04b">
+
+            <!-- 预留一个一级菜单, 将来通过v-for动态生成多组一级菜单 -->
+            <!-- 这里的index用来给当前菜单起个名字, 以区分 -->
+            <el-submenu v-for="item in menu" :key="item.title" :index="item.title" >
+
+                <!-- 菜单title -->
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>{{ item.title }}</span>
+                </template>
+
+                <!-- 菜单list, 遍历当前菜单的chidren拿数据 -->
+                <el-menu-item index="1-1" v-for="subitem in item.chidren" :key="subitem.title">
+                    <router-link :to="subitem.path">{{ subitem.title }}</router-link>
+                </el-menu-item>
+
+            </el-submenu>
+
+        </el-menu>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                // 左侧导航列表数据
+                menu: [
+                    { 
+                        title: '商品管理', 
+                        chidren: [ 
+                            { title: '内容管理', path: '/admin/goods' }, 
+                            { title: '类别管理', path: '/admin/category' }, 
+                            { title: '评论管理', path: '/admin/comment' } 
+                        ]
+                    },
+                    { 
+                        title: '订单管理', 
+                        chidren: [ 
+                            { title: '内容管理1', path: '/admin/a' }, 
+                            { title: '类别管理2', path: '/admin/b' }, 
+                            { title: '评论管理3', path: '/admin/c' } 
+                        ]
+                    }
+                ]
+            }
+        },
     }
 </script>
 
