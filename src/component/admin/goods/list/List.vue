be7210569a29213e98a54c264c1ce88f4fc07d11
PengFei <626138959@qq.com>
2018-03-03 11:58:03 +0800

    删除商品列表里没用的代码

--- src/component/admin/goods/list/List.vue
+++ src/component/admin/goods/list/List.vue
@@ -107,19 +107,8 @@
                         date: '2016-05-02',
                         name: '王小虎',
                         address: '上海市普陀区金沙江路 1518 弄'
-                    }, 
-                    {
-                        date: '2016-05-04',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, 
-                    {
-                        date: '2016-05-01',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
                     }
-                ],
-                multipleSelection: []
+                ]
             }
         },
 
@@ -161,20 +150,6 @@
             // 全选按钮
             all() {
                 document.querySelector('.el-checkbox__original').click();
-            },
-            
-            toggleSelection(rows) {
-                if (rows) {
-                    rows.forEach(row => {
-                        this.$refs.multipleTable.toggleRowSelection(row);
-                    });
-                } else {
-                    this.$refs.multipleTable.clearSelection();
-                }
-            },
-
-            handleSelectionChange(val) {
-                this.multipleSelection = val;
             }
         },
 
