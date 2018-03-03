2d72e7b32bdfdf7ca0eebdfe52c5778701ba8ace
PengFei <626138959@qq.com>
2018-03-03 09:41:57 +0800

    商品搜索功能

--- src/component/admin/goods/list/List.vue
+++ src/component/admin/goods/list/List.vue
@@ -15,7 +15,7 @@
 
             <div class="list_btns_right">
                 <el-input placeholder="请输入商品名称" prefix-icon="el-icon-search" 
-                    label-width="200px" size="mini">
+                    label-width="200px" size="mini" v-model="apiQuery.searchvalue" @blur="search">
                 </el-input>
             </div>
         </section>
@@ -65,6 +65,15 @@
     export default {
         data() {
             return {
+
+                // 搜索
+                apiQuery: {
+                    pageIndex: 1,
+                    pageSize: 10,
+                    searchvalue: ''
+                },
+
+                // 表格数据
                 tableData3: [
                     {
                         date: '2016-05-03',
@@ -93,10 +102,16 @@
 
         methods: {
 
+            // 搜索
+            search() {
+                this.getGoodsData();
+            },
+
             // 获取商品列表数据
             getGoodsData() {
                 // 这个接口需要pageIndex指定页, pageSize指定每页数量, searchvalue用于商品搜索
-                this.$http.get(this.$api.gsList + '?pageIndex=1&pageSize=10').then((res) => {
+                let api = `${this.$api.gsList}?pageIndex=${this.apiQuery.pageIndex}&pageSize=${this.apiQuery.pageSize}&searchvalue=${this.apiQuery.searchvalue}`
+                this.$http.get(api).then((res) => {
                     if(res.data.status == 0) {
                         this.tableData3 = res.data.message;  // 把请求回来的数据覆盖原data数量, 表格就会自动刷新
                     }
