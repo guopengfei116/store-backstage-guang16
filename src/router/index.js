140502f0ac3fdceff81ecf3e24c85e42f7594c1a
PengFei <626138959@qq.com>
2018-03-05 10:41:25 +0800

    页面添加登陆校验

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const goods = [
 ];
 
 // 导出路由实例 
-export default new VueRouter({
+let router = new VueRouter({
     routes: [
         // 登陆
         { name: 'login', path: '/login', component: Login },
@@ -29,3 +29,45 @@ export default new VueRouter({
         { name: 'admin', path: '/admin', component: Admin, children: [...goods] },
     ]
 });
+
+// 添加路由前置守卫, 其实就是添加一个函数, 这个函数会在路由配置之前执行, 我们可以在这里添加一些登陆或者权限的校验
+// to与from是两个对象, 可以拿到url信息
+router.beforeEach((to, from, next) => {
+    // 通过to.name得知用户访问的是什么页面, 如果是admin相关页面, 那么继续判断用户有没有登陆
+    // 有 => 调用next()通过访问,  没有 => 调用next({name: 'login'})跳转到登陆页面
+
+    
+    // 现在我们是在一个js模块当中, 并不是在vue组件中, 所以这里的this不是组件实例, 而是undefined, 无法this.$http与this.$api
+    Vue.prototype.$http.get(Vue.prototype.$api.islogin).then(res => {
+        let isLogin = false;
+
+        // 已登陆
+        if(res.data.code == 'logined') {
+            isLogin = true;
+        }
+
+        // 如果访问登陆页面
+        // 已登陆 => 为了用户友好体验, 自动跳转到后台管理
+        // 未登陆 => 允许访问登陆页面
+        if(to.name == 'login') {
+            if(isLogin) {
+                next({name: 'admin'});
+            }else {
+                next();
+            }
+        }
+
+        // 如果访问后台页面
+        // 已登陆 => 允许访问后台页面 
+        // 未登陆 => 禁止访问, 自动跳转到登陆页面
+        if(to.name != 'login') {
+            if(isLogin) {
+                next();
+            }else {
+                next({name: 'login'});
+            }
+        }
+    })
+});
+
+export default router;
